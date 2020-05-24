d0c939be4d4c12bb744aa41e40c4b856155061c0
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-23 22:45:44 -0500

    add credit duration distribution file - display dataframe credit

---
+++ credit_duration_distribution.R
@@ -0,0 +1,13 @@
+# Instalación de paquetes
+# install.packages("Fahrmeir")
+# install.packages("ggplot2")
+
+# Cargamos base de datos: credit
+library(Fahrmeir)
+data(credit)
+
+# Mostramos primeros 6 registros de la base de datos
+head(credit)
+
+# Mostramos estructura de la base de datos: data.frame
+str(credit)
