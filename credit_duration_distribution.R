d6db219115c670ef0cb4ae993ea35d0e00760263
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-23 23:02:25 -0500

    show graphic dist credit month

--- credit_duration_distribution.R
+++ credit_duration_distribution.R
@@ -2,12 +2,39 @@
 # install.packages("Fahrmeir")
 # install.packages("ggplot2")
 
-# Cargamos base de datos: credit
+# Cargamos el data frame: credit
 library(Fahrmeir)
 data(credit)
 
-# Mostramos primeros 6 registros de la base de datos
+# Mostramos primeros 6 registros del data frame
 head(credit)
 
-# Mostramos estructura de la base de datos: data.frame
+# Mostramos estructura de la base de datos: data frame
 str(credit)
+
+# Resumen de las variables del data frame
+summary(credit)
+
+# Analizando la variable "Mes": Duración del Préstamo
+
+# Obtenimieno la frecuencia absoluta
+ni<-table(credit$Mes)
+ni
+
+# Obteniendo la frecuencia relativa
+fi<-prop.table(table(credit$Mes))
+fi
+
+# Obteniendo la frecuencua porcentual
+pi<-fi*100
+pi
+
+# Visualizando distribución de la variable "Mes"
+
+# Gráfico de Barras
+barplot(
+  pi,
+  col='black',
+  main="Distribución de la duración de los préstamos",
+  xlab="Meses de Préstamo",
+  ylab="Proporción Porcentual")
